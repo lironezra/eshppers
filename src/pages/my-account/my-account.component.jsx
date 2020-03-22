ef031cc6ba501546ccf9a1c7d29e1dd15a6a1bd3
lironezra <lironezra1@gmail.com>
2020-03-22 15:54:43 +0200

    Edit My-Account page with list of options for manage user account settings like changing email, display name, password

--- src/pages/my-account/my-account.component.jsx
+++ src/pages/my-account/my-account.component.jsx
@@ -1,24 +1,32 @@
 import React, { useEffect, useState } from 'react';
-import { Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { NavLink, Route, Switch } from 'react-router-dom';
+
+import { getAuthenticatedUser } from '../../redux/user/user.actions'
+
+import AccountOverview from '../../components/my-account-overview-components/account-overview.component';
+import MyOrders from '../../components/my-account-overview-components/my-orders.component';
+import MyDetails from '../../components/my-account-overview-components/my-details.component';
 
 import AppLogo from '../../assets/app-logo.png';
 import NortonSecureLogo from '../../assets/norton-by-symantec-logo.png';
-import WelcomeUserPng from '../../assets/male-friendship.jpg'; 
-
 
 import './my-account.styles.scss';
 
-const MyAccountPage = () => {
+const MyAccountPage = ({ match, user, getCurrentUser }) => {
     const [opacity, setOpacity] = useState(false);
     let style = { opacity: 0 }
+
     useEffect(() => {
         // change page background color
         document.body.style.backgroundColor = '#eee';
         setOpacity(true);
+        getCurrentUser();
+
         return () => {
             document.body.style.backgroundColor = 'white';
         };
-    }, []);
+    }, [getCurrentUser]);
 
     if(opacity) { 
         style = { opacity: 1 }
@@ -42,24 +50,60 @@ const MyAccountPage = () => {
                 <div className='user-options-container'>
                     <div className='user-welcome-card'>
                         <div className='dot'>
-                            <p>LE</p>
+                            <p>
+                                {user ? user.displayName.match(/\b(\w)/g).join('') : null}
+                            </p>
                         </div>
                         <div className='user-welcome-wrapper'>
                             <p className='hi'>Hi,</p>
-                            <p className='name'>liron ezra</p>
+                            <p className='name'>{user ? user.displayName : null}</p>
                         </div>
                     </div>
+                    <div className='user-side-menu'>
+                        <nav className='main-nav'>
+                            <ul className='main-list'>
+                                {console.log(match.url)}
+                                <NavLink exact to='/user-account' className='main-list-item' activeClassName="selectedLink">Account overview</NavLink>
+                                <NavLink to={`${match.url}/orders`} className='main-list-item' activeClassName="selectedLink">My orders</NavLink>
+                                <NavLink to={`${match.url}/my-details`} className='main-list-item' activeClassName="selectedLink">My details</NavLink>
+                                <NavLink to='/change-password' className='main-list-item' activeClassName="selectedLink">Change password</NavLink>
+                                <NavLink to='/addresses' className='main-list-item' activeClassName="selectedLink">Address book</NavLink>
+                                <NavLink to='/payment-methods' className='main-list-item' activeClassName="selectedLink">Payment method</NavLink>
+                                <NavLink to='/logout' className='main-list-item' activeClassName="selectedLink">Sign out</NavLink>
+                            </ul>
+                        </nav>
+                    </div>
                 </div>
-                <div className='welcome-user'>
-                    <img className='welcome-img' src={WelcomeUserPng} alt='male-friendship' />
-                    <h1 className='welcome-user-text'>
-                        <span>WELCOME TO</span>
-                        <span>YOUR ACCOUNT</span>
-                    </h1>
+                <div className='selected-user-option'>
+                     <Switch>
+                        <Route exact path='/user-account'>
+                            <AccountOverview />    
+                        </Route> 
+                        <Route path='/user-account/orders'>
+                            <MyOrders />
+                        </Route>
+                        <Route path='/user-account/my-details'>
+                            <MyDetails />
+                        </Route>
+                     </Switch>
                 </div>
             </div>
+
         </div>
     );
 }
 
-export default MyAccountPage;
+const mapStateToProps = state => {
+    return {
+        //userDisplayName: state.auth.userDisplayName,
+        user: state.user.user
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        getCurrentUser: () => dispatch(getAuthenticatedUser())
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(MyAccountPage);
